a93b00ed7a60aadfde3087c5b52626364899b05e
IzzyVIII <eagb08@gmail.com>
2024-03-12 11:43:58 -0500

    First Commit

---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,11 @@
+import * as _React from 'react'; 
+// import React from 'react'; 
+
+
+export const Cart = () => {
+    return (
+        <div>
+            <h1 style={{marginTop: '50px'}}>This is our Cart</h1>
+        </div>
+    )
+}
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,59 @@
+import * as _React from 'react'; 
+import { styled } from '@mui/system'; 
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom'; 
+
+// Internal Imports
+import shopImage from '../../assets/Images/red_vespa.jpeg'
+import { NavBar } from '../sharedComponents';
+
+// make a simple interface
+interface Props {
+    title: string
+}
+
+
+// code out our styled components
+const Root = styled('div')({
+    padding: 0,
+    margin: 0
+})
+
+
+const Main = styled('main')({
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .5)), url(${shopImage});`,
+    width: '100%',
+    height: '100%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center top 5px', 
+    position: 'absolute',
+    marginTop: '10px'
+})
+
+const MainText = styled('div')({
+    textAlign: 'center',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white'
+})
+
+
+// This is our first functional based component!
+export const Home = (props: Props) => {
+
+    // return is always HTML & it can have ONLY 1 parent div 
+    return (
+        <Root>
+            <NavBar/>
+            <Main>
+                <MainText>
+                    <Typography variant='h3'> { props.title }</Typography>
+                    <Button sx={{ marginTop: '10px'}} component={Link} to={"/shop"} variant='contained'>Make Your Scooter Unique Here!</Button>
+                </MainText>
+            </Main>
+        </Root>
+    )
+}
